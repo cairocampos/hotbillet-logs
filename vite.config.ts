f07e226254032ced7bc035b317316685faa92be6
Cairo Campos <cairocampos98@gmail.com>
2022-05-17 13:55:55 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "logs",
+  base: "/logs",
   plugins: [vue()],
   resolve: {
     alias: {
